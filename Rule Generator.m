13f4c57f1100c23f2cdecf29da9a152d64f14ce9
tleining1 <144941471+tleining1@users.noreply.github.com>
2026-01-23 18:57:11 -0800

    adding fix for issue where operator on lhs of rule needs to be general operator but needs to be tensor in end

--- Rule Generator.m
+++ Rule Generator.m
@@ -2,10 +2,12 @@
 
 IndexCombos=Map[Flatten[#]&,Join[Tuples[{{a,-a},Permutations[{b,-b}],Permutations[{c,-c}]}]]];
 
+NegativeQ[a_]:=MatchQ[a,Times[-1,_]]
+
 PatternAll[a_]:=Module[{neg},
 neg=NegativeQ[a];
 ReleaseHold[If[neg, Times[-1,Pattern[Evaluate[-1*a],Blank[]]],Pattern[Hold[a],Blank[]]]]
 ]
 
-RuleAGP=Map[With[{Under=Join[Map[PatternAll[#]&,#]]},G[Under[[1]],Under[[2]],Under[[4]]]*Op1_[j__,Under[[5]]]*Op_[Under[[3]]]:>epsilong[#[[2]],#[[4]]]CD[#[[3]]]@Op[] CD[#[[5]]]@Op1[j]
+RuleAGP[IndexCombos_]:=Map[With[{Under=Join[Map[PatternAll[#]&,#]]},Gg_[Under[[1]],Under[[2]],Under[[4]]]*Op1_[j__,Under[[5]]]*Op_[Under[[3]]]:>If[Gg==G,epsilong[#[[2]],#[[4]]]CD[#[[3]]]@Op[] CD[#[[5]]]@Op1[j]]
 ]&,IndexCombos]
