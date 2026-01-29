099524529ba6457f24db2626e1a4655e4d6cab34
tleining1 <144941471+tleining1@users.noreply.github.com>
2026-01-29 13:34:41 -0800

    adding rules for current

--- Rule Generator.m
+++ Rule Generator.m
@@ -8,7 +8,7 @@ PatternAll[a_]:=Module[{neg},
 neg=NegativeQ[a];
 ReleaseHold[If[neg, Times[-1,Pattern[Evaluate[-1*a],Blank[]]],Pattern[Hold[a],Blank[]]]]
 ]
-
+(*stress tensor/ no color index/ two free spactime index rules*)
 RuleAGP[IndexCombos_]:=Map[With[{Under=Join[Map[PatternAll[#]&,#]]},Gg_[Under[[1]],Under[[2]],Under[[4]]]*Op1_[j__,Under[[5]]]*Op_[Under[[3]]]:>If[Gg==G,epsilong[#[[2]],#[[4]]]CD[#[[3]]]@Op[] CD[#[[5]]]@Op1[j]]
 ]&,IndexCombos]
 
@@ -18,3 +18,12 @@ RuleGAA[IndexCombos_]:=Map[With[{Under=Join[Map[PatternAll[#]&,#]]},Gg_[Under[[1
 RuleDA={CD1_[\[Mu]_]@Op_[\[Nu]_,a_]:>If[Op==A&&CD1==CD,CD1[\[Mu]]@Op[\[Nu]]]}
 
 RuleDP={CD1_[\[Mu]_]@Op_[a_]:>If[Op==\[Phi]&&CD1==CD,CD1[\[Mu]]@Op[]]}
+
+(*conserved current/ 1 color color index/ one free spacetime index*)
+RuleGGAAA[\[Mu]_,a_]:=Module[{b,c,d,e,\[Nu],\[Rho],\[Sigma],\[Gamma],\[Delta]},Op_[\[Nu]_,-e_]Gg_[-b_,a,e_]Op_[-\[Nu]_,-d_]Op_[\[Mu],-c_]Gg_[b_,c_,d_]:>If[Gg==G,epsilong[\[Rho],\[Sigma]]CD[-\[Rho]]@Op[\[Nu]]CD[-\[Sigma]]@(epsilong[\[Gamma],\[Delta]]CD[-\[Gamma]]@Op[\[Mu]]CD[-\[Delta]]@Op[-\[Nu]])]]
+
+RuleGADA[\[Mu]_,a_]:=Module[{\[Nu],b,c,\[Rho],\[Sigma]},Op_[\[Nu]_,-c_]Gg_[-b_,a,c_]CD1_[\[Mu]]@Op_[-\[Nu]_,b_]:>If[Gg==G&&CD1==CD,epsilong[\[Rho],\[Sigma]]CD1[-\[Rho]]@Op[\[Nu]]CD1[-\[Sigma]]@CD[\[Mu]]@Op[-\[Nu]]]]
+
+RuleGADA1[\[Mu]_,a_]:=Module[{\[Nu],b,c,\[Rho],\[Sigma]},Op_[\[Nu]_,-c_]Gg_[-b_,a,c_]CD1_[-\[Nu]_]@Op_[\[Mu],b_]:>If[Gg==G&&CD1==CD,epsilong[\[Rho],\[Sigma]]CD1[-\[Rho]]@Op[\[Nu]]CD1[-\[Sigma]]@CD[-\[Nu]]@Op[\[Mu]]]]
+
+RuleGGAPP[\[Mu]_,a_]:=Module[{b,c,d,e,\[Rho],\[Sigma],\[Gamma],\[Delta]},Gg_[b_,a,e_]Gg_[-b_,c_,d_]Op_[\[Mu],-d_]Op1_[-e_]Op1_[-c_]:>epsilong[\[Rho],\[Sigma]]CD[-\[Rho]]@Op1[]CD[-\[Sigma]]@(epsilong[\[Gamma],\[Delta]]CD[-\[Gamma]]@Op1[]CD[-\[Delta]]@Op[\[Mu]])]
