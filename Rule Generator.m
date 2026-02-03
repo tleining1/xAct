56f80a35ea70f38a0ea50eadc9e5a250e1b18cf9
tleining1 <144941471+tleining1@users.noreply.github.com>
2026-02-03 11:29:53 -0800

    General ruleswith tripple underscore from Jan 30

--- Rule Generator.m
+++ Rule Generator.m
@@ -27,3 +27,12 @@ RuleGADA[\[Mu]_,a_]:=Module[{\[Nu],b,c,\[Rho],\[Sigma]},Op_[\[Nu]_,-c_]Gg_[-b_,a
 RuleGADA1[\[Mu]_,a_]:=Module[{\[Nu],b,c,\[Rho],\[Sigma]},Op_[\[Nu]_,-c_]Gg_[-b_,a,c_]CD1_[-\[Nu]_]@Op_[\[Mu],b_]:>If[Gg==G&&CD1==CD,epsilong[\[Rho],\[Sigma]]CD1[-\[Rho]]@Op[\[Nu]]CD1[-\[Sigma]]@CD[-\[Nu]]@Op[\[Mu]]]]
 
 RuleGGAPP[\[Mu]_,a_]:=Module[{b,c,d,e,\[Rho],\[Sigma],\[Gamma],\[Delta]},Gg_[b_,a,e_]Gg_[-b_,c_,d_]Op_[\[Mu],-d_]Op1_[-e_]Op1_[-c_]:>epsilong[\[Rho],\[Sigma]]CD[-\[Rho]]@Op1[]CD[-\[Sigma]]@(epsilong[\[Gamma],\[Delta]]CD[-\[Gamma]]@Op1[]CD[-\[Delta]]@Op[\[Mu]])]
+
+RuleGPDP[\[Mu]_,a_]:=Module[{b,c,\[Rho],\[Sigma]},Gg_[b_,a,c_]Op_[-c_]CD1_[\[Mu]]@Op_[-b_]:>If[Gg==G&&CD1==CD,epsilong[\[Rho],\[Sigma]]CD1[-\[Rho]]@Op[]CD1[-\[Sigma]]@CD1[\[Mu]]@Op[]]]
+
+(*General rules*)
+
+RuleGen[IndexCombos]:=Map[With[{Under=Join[Map[PatternAll[#]&,#]]},Gg_[Under[[1]],Under[[2]],Under[[4]]]*Op1_[j___,Under[[5]]]*Op_[k___,Under[[3]]]:>If[Gg==G,epsilong[#[[2]],#[[4]]]CD[#[[3]]]@Op[k] CD[#[[5]]]@Op1[j]]
+]&,IndexCombos]
+
+RuleA={CD1_[\[Mu]_]@Op_[\[Nu]___,a_]:>If[CD1==CD,CD1[\[Mu]]@Op[\[Nu]]]}
